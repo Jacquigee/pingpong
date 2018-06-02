45559818acd67ae7d602bbce48d8f69d2ddbbb2d
jacqui <gitaujaquiline@gmail.com>
2018-06-02 09:55:26 +0300

    Add business logic in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+function myFunction(i,output) {
+  for (var i = 1; i <= num; i++) {
+    event.preventDefault();
+
+  if (i % 3 === 0) && (i % 5 ===0) {
+    output.push("PingPong");
+  }
+  else if (i % 3 === 0){
+    output.push("Ping");
+  }
+  else if (i % 5 === 0) {
+    output.push("Pong");
+  }
+  else {
+    output.push(i);
+  }
+
+  return output;
+}
+}
